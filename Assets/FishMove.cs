b60294c78b6f22709e2dfdb2b3f51d1f892d265b
Gustav <sir.gustav.the.coder@gmail.com>
2017-11-29 22:01:26 +0100

    Swimming fishes

--- Assets/FishMove.cs
+++ Assets/FishMove.cs
@@ -5,12 +5,40 @@ using UnityEngine;
 public class FishMove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
+		t = Random.value;
 	}
 
+	float y = 0;
+	int mul = 1;
+	float t = 0;
+
 	float Speed = 1;
+	float SinSize = 0.1f;
+
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = this.transform.position + new Vector3(1, 0, 0)*this.Speed * Time.deltaTime;
+		this.t += Time.deltaTime;
+		if(t > 1) t-= 1;
+
+		var p = this.transform.position + new Vector3(1, 0, 0)*this.Speed * Time.deltaTime * this.mul;
+		p.y = y + Mathf.Sin(Mathf.PI * 2 * t) * SinSize;
+		this.transform.position = p;
+
+
+		if(this.transform.position.x > 10 && mul == 1) {
+			Object.Destroy(this.gameObject);
+		}
+		if(this.transform.position.x < -10 && mul == -1) {
+			Object.Destroy(this.gameObject);
+		}
+	}
+
+	public void SetRight(bool right)
+	{
+		var sp = this.GetComponent<SpriteRenderer>();
+		sp.flipX = right;
+		this.mul = right ? -1 : 1;
+		this.y = this.transform.position.y;
 	}
 }
---
+++ Assets/FishSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	float d = 0;
+
+
+	float spawn = 1.0f;
+	float offsetx = 10;
+	float offsety = 3;
+
+	public FishMove fish_template;
+	
+	// Update is called once per frame
+	void Update () {
+		d -= Time.deltaTime;
+		if(d < 0 ) {
+			SpawnFish();
+			d += spawn;
+		}
+	}
+
+	void SpawnFish()
+	{
+		var fish = GameObject.Instantiate<FishMove>(fish_template);
+		var right = Random.value > 0.5f;
+		var dx = (right?1:-1) * offsetx;
+		var dy = Random.Range(-offsety, offsety);
+		fish.transform.position = this.transform.position + new Vector3(dx, dy);
+		fish.SetRight(right);
+	}
+}
