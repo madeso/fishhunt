75483c6eebb835d9f3f3e27fd8e2fb6cc937d334
Gustav <sir.gustav.the.coder@gmail.com>
2017-11-30 07:10:42 +0100

    bubble

---
+++ Assets/Bubble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		weight = Random.Range(0.5f, 1.0f);
+	}
+
+	float weight = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		var p = this.transform.position;
+		p.y += Time.deltaTime * weight;
+		if(p.y > 6) {
+			GameObject.Destroy(this.gameObject);
+		}
+		this.transform.position = p;
+	}
+}
---
+++ Assets/BubbleSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour {
+	public Bubble[] Bubbles;
+
+	float RandomX()
+	{
+		return Random.Range(-offsetx, offsetx);
+	}
+
+	float RandomY()
+	{
+		return Random.Range(-offsety, offsety);
+	}
+
+	void Start () {
+		for(int i=0; i<10; i+=1)
+		{
+			SpawnAt(RandomX(), RandomY());
+		}
+	}
+
+	float offsetx = 10;
+	float offsety = 4;
+
+	void SpawnAt(float x, float y) {
+		var index = Random.Range(0, Bubbles.Length-1);
+		var template = Bubbles[index];
+		var bubble = GameObject.Instantiate<Bubble>(template);
+		bubble.transform.position = new Vector3(x, y, 0);
+	}
+
+	void SpawnAtBottom()
+	{
+		SpawnAt(RandomX(), -offsety);
+	}
+
+	float timer = 0;
+
+	void Update () {
+		timer -= Time.deltaTime * 1.5f;
+		if(timer < 0 ) {
+			timer += 1;
+			SpawnAtBottom();
+		}
+	}
+}
