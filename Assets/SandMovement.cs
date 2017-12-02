752428f2e5c1c013426ff3140fbaa7cd616e0dbe
Gustav <sir.gustav.the.coder@gmail.com>
2017-12-02 23:45:51 +0100

    Moving background

---
+++ Assets/SandMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandMovement : MonoBehaviour {
+	public float d = 0;
+
+	float size = 0.1f;
+	float speed = 0.3f;
+
+	float s;
+
+	// Use this for initialization
+	void Start () {
+		s = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		d += Time.deltaTime * speed;
+		if(d > 1) d-=1;
+		var np = this.transform.position;
+		np.y = s + size * Mathf.Sin(d * Mathf.PI * 2f);
+		this.transform.position = np;
+	}
+}
