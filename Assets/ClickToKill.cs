e7175015b21dae7da593ae5c353e7bb6d740b69b
Gustav <sir.gustav.the.coder@gmail.com>
2017-12-02 19:52:50 +0100

    Skeletons spawn when fishes are killed

--- Assets/ClickToKill.cs
+++ Assets/ClickToKill.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClickToKill : MonoBehaviour {
+	public Skeleton Skeleton;
+
+	SpriteRenderer rend;
 
 	// Use this for initialization
 	void Start () {
-		
+		rend = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -16,5 +19,16 @@ public class ClickToKill : MonoBehaviour {
 	public void Kill()
 	{
 		Object.Destroy(this.gameObject);
+
+		if(this.Skeleton == null)
+		{
+			Debug.Log(string.Format("No skeleton for {0}", this.name));
+		}
+		else
+		{
+			Skeleton sk = GameObject.Instantiate(Skeleton);
+			sk.gameObject.transform.position = this.transform.position;
+			sk.SetOrientation(rend.flipX);
+		}
 	}
 }
---
+++ Assets/Skeleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour {
+	SpriteRenderer rend;
+
+	bool flipped;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer>();
+		rend.flipX = flipped;
+	}
+
+	float speed = 0.5f;
+	
+	// Update is called once per frame
+	void Update () {
+		var p = this.transform.position;
+		p.y -= Time.deltaTime * speed;
+		if(p.y < -5 )
+		{
+			// GameObject.Destroy(this);
+		}
+		this.transform.position = p;
+	}
+
+	public void SetOrientation(bool flipx)
+	{
+		this.flipped = flipx;
+	}
+}
